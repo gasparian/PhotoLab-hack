fa9336103caf22c11ae1d8aff06c6c513bce3df7
yustina.ivanova <yustina.ivanova@texel.graphics>
2019-01-27 15:48:20 +0300

    three buttons

--- flask/app.py
+++ flask/app.py
@@ -8,8 +8,10 @@ app.debug=True
 
 UPLOAD_FOLDER_SELFIE = os.path.basename('uploads')
 UPLOAD_FOLDER_CROWD = os.path.basename('images')
+UPLOAD_FOLDER_MIX = os.path.basename('mix')
 app.config['UPLOAD_FOLDER_SELFIE'] = UPLOAD_FOLDER_SELFIE
 app.config['UPLOAD_FOLDER_CROWD'] = UPLOAD_FOLDER_CROWD
+app.config['UPLOAD_FOLDER_MIX'] = UPLOAD_FOLDER_MIX
 
 @app.route('/')
 def hello_world():
@@ -17,27 +19,45 @@ def hello_world():
 
 @app.route('/upload_selfie', methods=['POST'])
 def upload_file_selfie():
-    file = request.files['image']
+    file = request.files['image_selfie']
     f = os.path.join(app.config['UPLOAD_FOLDER_SELFIE'], file.filename)
-    
     # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
     file.save(f)
-
+    file_for_save = open("file_selfie.txt","w+")
+    file_for_save.write(file.filename)
+    file_for_save.close()
     return render_template('index.html', uploaded_selfie_success=True, init=True)
 
 @app.route('/upload_crowd',  methods=['GET', 'POST'])
 def upload_file_crowd():
-    id_selected = request.form.get('id_selected')
-    #filename = os.path.join(app.config['UPLOAD_FOLDER_CROWD'], str(id_selected) + '.jpg')
-    filename = "./images/" + str(id_selected) + '.jpg'
-	
-    #here is script for image transform.
-    #new_loc = os.path.join(app.config['UPLOAD_FOLDER_CROWD'], 'new.jpg')
-	#im_new.save(new_loc,"PNG")
-	
-    return send_file(app.config['UPLOAD_FOLDER_CROWD']+"/"+str(id_selected) + '.jpg', 
-					 as_attachment=True, 
-					 attachment_filename=str(id_selected) + '.jpg')
+    file = request.files['image_crowd']
+    f = os.path.join(app.config['UPLOAD_FOLDER_CROWD'], file.filename)
+    # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
+    file.save(f)
+    file_for_save = open("file_crowd.txt","w+")
+    file_for_save.write(file.filename)
+    file_for_save.close()
+    return render_template('index.html', uploaded_crowd_success=True, init=True)
+
+@app.route('/create_mix',  methods=['GET', 'POST'])
+def upload_create_mix():
+    file_saved_selfie = open("file_selfie.txt","r")
+    file_selfie_str = str(file_saved_selfie.readline())
+    file_saved_selfie.close()
+    file_saved_crowd = open("file_crowd.txt","r")
+    file_crowd_str = str(file_saved_crowd.readline())
+    file_saved_crowd.close()
+    file_selfie = os.path.join(app.config['UPLOAD_FOLDER_SELFIE'], file_selfie_str)
+    file_crowd = os.path.join(app.config['UPLOAD_FOLDER_CROWD'], file_crowd_str)
+    im_selfie = Image.open(file_selfie)
+    im_crowd = Image.open(file_crowd)
+
+    im = im_crowd
+    file = os.path.join(app.config['UPLOAD_FOLDER_MIX'])
+    im.save(file + "/result", "JPEG")
+    return render_template('index.html', created_success=True, init=True)
+
+
 
 if __name__ == '__main__':
     app.run()
