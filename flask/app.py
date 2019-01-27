51ce00d96e10f6aaeb97146d7c8e925721c4900a
yustina.ivanova <yustina.ivanova@texel.graphics>
2019-01-27 14:19:45 +0300

    commit before deleting

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
+from PIL import Image
+import numpy as np
 
 app = Flask(__name__)
 app.debug=True
@@ -23,8 +25,19 @@ def upload_file_selfie():
 
     return render_template('index.html', uploaded_selfie_success=True, init=True)
 
-@app.route('/upload_crowd', methods=['POST', 'GET'])
+@app.route('/upload_crowd',  methods=['GET', 'POST'])
 def upload_file_crowd():
     id_selected = request.form.get('id_selected')
-    print('id_selected', id_selected)
-    return render_template('index.html', uploaded_crowd_success=True, init=True)
+    #filename = os.path.join(app.config['UPLOAD_FOLDER_CROWD'], str(id_selected) + '.jpg')
+    filename = "./images/" + str(id_selected) + '.jpg'
+	
+    #here is script for image transform.
+    #new_loc = os.path.join(app.config['UPLOAD_FOLDER_CROWD'], 'new.jpg')
+	#im_new.save(new_loc,"PNG")
+	
+    return send_file(app.config['UPLOAD_FOLDER_CROWD']+"/"+str(id_selected) + '.jpg', 
+					 as_attachment=True, 
+					 attachment_filename=str(id_selected) + '.jpg')
+
+if __name__ == '__main__':
+    app.run()
