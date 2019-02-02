edec93d8f77c246706b3dd768d6fa69115901ceb
root <root@ml.multivi.local>
2019-02-03 00:23:18 +0100

    bunch of selfies+points as input

--- app.py
+++ app.py
@@ -68,6 +68,19 @@ def select_faces(im, bbox, r=10):
 def calc_dist(img0, img1):
     return distance.euclidean(img0, img1)
 
+def get_selfie_bboxs(me):
+    my_bboxs = np.array(face_detection(me[0]))
+    # points on the faces to be swapped (selfies / friends photos)
+    if me[1] is not None:
+        to_keep = []
+        for arg in me[1]:
+            for j, bbox in enumerate(my_bboxs):
+                if (bbox[0] <= arg[0] <= bbox[2]) and \
+                   (bbox[1] <= arg[1] <= bbox[3]):
+                        to_keep.append(j)
+        my_bboxs = my_bboxs[to_keep]
+    return my_bboxs
+
 def chunker(l, n):
     length = len(l)
     n = length // n
@@ -81,66 +94,63 @@ def chunker(l, n):
 class preprocess_img:
 
     @classmethod
-    def run(self, max_dst_boxes=25, embeddings_max_iters=2, n_jobs=2, *args):
-        
+    def run(self, CROWD, SELFIES, max_dst_boxes=25, embeddings_max_iters=2, n_jobs=2):
+        #args is a list of tuples: (img, points)
+       
+        self.CROWD = CROWD 
         self.embeddings_max_iters = embeddings_max_iters
-        my_bboxs = np.array(face_detection(ME))
-        
-        # args is points on the faces to be swapped (selfies / friends photos)
-        if args:
-            to_keep = []
-            for arg in args:
-                for j, bbox in enumerate(my_bboxs):
-                    if (bbox[0] <= arg[0] <= bbox[2]) and \
-                       (bbox[1] <= arg[1] <= bbox[3]):
-                            to_keep.append(j)
-            my_bboxs = my_bboxs[to_keep]
 
+        selfies_boxes = []
+        for SELFIE in SELFIES:
+            selfies_boxes.append(get_selfie_bboxs(SELFIE))
+
+        selfies_boxes_len = sum([len(my_bboxs) for my_bboxs in selfies_boxes])
         self.bboxs = np.array(face_detection(CROWD))
-        if len(self.bboxs) == 0 or len(my_bboxs) == 0:
+        if len(self.bboxs) == 0 or selfies_boxes_len == 0:
             return None
 
         random_sample_bboxs = np.random.choice(list(range(len(self.bboxs))), 
                                                size=min(len(self.bboxs), max_dst_boxes))
-        random_sample_my_bboxs = np.random.choice(list(range(len(my_bboxs))), 
-                                               size=min(len(my_bboxs), max_dst_boxes))
+        random_sample_my_bboxs = np.random.choice(list(range(selfies_boxes_len)), 
+                                               size=min(selfies_boxes_len, max_dst_boxes))
 
         if len(random_sample_bboxs) < len(self.bboxs):
             self.bboxs = self.bboxs[random_sample_bboxs]
 
-        if len(random_sample_my_bboxs) < len(my_bboxs):
+        if len(random_sample_my_bboxs) < selfies_boxes_len:
             my_bboxs = my_bboxs[random_sample_my_bboxs]
 
         if len(self.bboxs) < 2:
             n_jobs = 1
             
         out, self.ignore_list = [], []
-        for i in range(len(my_bboxs)):
-            self.src_face_descriptor = FACEREC.compute_face_descriptor(ME, 
-                                  SP(ME, dlib.rectangle(*my_bboxs[i])), embeddings_max_iters)
+        for img_num, SELFIE in enumerate(SELFIES):
+            my_bboxs = selfies_boxes[img_num]
+            for i in range(len(my_bboxs)):
+                self.src_face_descriptor = FACEREC.compute_face_descriptor(SELFIE[0], 
+                                      SP(SELFIE[0], dlib.rectangle(*my_bboxs[i])), embeddings_max_iters)
 
-            if n_jobs == 1:
-                dsts = np.full(len(bboxs), np.inf)
-                for j, bbox in enumerate(bboxs):
-                    if j in ignore_list:
-                        continue
-                    face_descriptor = FACEREC.compute_face_descriptor(CROWD,
-                                      SP(CROWD, dlib.rectangle(*bbox)), embeddings_max_iters)
-                    dsts[j] = calc_dist(src_face_descriptor, face_descriptor)
-            else:
-                manager = multiprocessing.Manager()
-                pool = multiprocessing.Pool(n_jobs)
-                res = manager.list([0] * n_jobs)
-                chunks = list(chunker(list(range(len(self.bboxs))), n_jobs))
-                pool.map(self.run_comparison, [(j, chunks[j], res) for j in range(n_jobs)])
-                dsts = np.concatenate([res[j] for j in range(n_jobs)], axis=0)
-                pool.close()
-                gc.collect()
+                if n_jobs == 1:
+                    dsts = np.full(len(self.bboxs), np.inf)
+                    for j, bbox in enumerate(self.bboxs):
+                        if j in self.ignore_list:
+                            continue
+                        face_descriptor = FACEREC.compute_face_descriptor(CROWD,
+                                          SP(CROWD, dlib.rectangle(*bbox)), embeddings_max_iters)
+                        dsts[j] = calc_dist(src_face_descriptor, face_descriptor)
+                else:
+                    manager = multiprocessing.Manager()
+                    pool = multiprocessing.Pool(n_jobs)
+                    res = manager.list([0] * n_jobs)
+                    chunks = list(chunker(list(range(len(self.bboxs))), n_jobs))
+                    pool.map(self.run_comparison, [(j, chunks[j], res) for j in range(n_jobs)])
+                    dsts = np.concatenate([res[j] for j in range(n_jobs)], axis=0)
+                    pool.close()
+                    gc.collect()
 
-            #clst = np.random.choice(np.argsort(dsts)[:2])
-            clst = np.argmin(dsts)
-            self.ignore_list.append(clst)
-            out.append((select_faces(CROWD, self.bboxs[clst]), select_faces(ME, my_bboxs[i])))
+                clst = np.argmin(dsts)
+                self.ignore_list.append(clst)
+                out.append((select_faces(CROWD, self.bboxs[clst]), select_faces(SELFIE[0], my_bboxs[i])))
         return out
     
     @classmethod
@@ -150,41 +160,36 @@ class preprocess_img:
         for l, m in enumerate(chunk):
             if m in self.ignore_list:
                 continue
-            face_descriptor = FACEREC.compute_face_descriptor(CROWD,
-                              SP(CROWD, dlib.rectangle(*self.bboxs[m])), self.embeddings_max_iters)
+            face_descriptor = FACEREC.compute_face_descriptor(self.CROWD,
+                              SP(self.CROWD, dlib.rectangle(*self.bboxs[m])), self.embeddings_max_iters)
             dsts[l] = calc_dist(self.src_face_descriptor, face_descriptor)
         res[k] = dsts
         del dsts; gc.collect()
 
-def insert_face():
+def insert_face(result, CROWD):
     
-    result = preprocess_img.run()
     if result is None:
         return None
-    
+ 
     result_bboxs = []
     for faces in result:
         dst_points, dst_shape, dst_face = faces[0]
         src_points, src_shape, src_face = faces[1]
 
-        warp_2d = False
-        correct_color = True
-        max_points = 58
-
         w, h = dst_face.shape[:2]
 
         ### Warp Image
-        if not warp_2d:
+        if not WARP_2D:
             ## 3d warp
-            warped_src_face = warp_image_3d(src_face, src_points[:max_points], dst_points[:max_points], (w, h))
+            warped_src_face = warp_image_3d(src_face, src_points[:MAX_POINTS], dst_points[:MAX_POINTS], (w, h))
         else:
             ## 2d warp
             src_mask = mask_from_points(src_face.shape[:2], src_points, radius=2)
             src_face = apply_mask(src_face, src_mask)
             # Correct Color for 2d warp
-            if correct_color:
-                warped_dst_img = warp_image_3d(dst_face, dst_points[:max_points], 
-                                               src_points[:max_points], src_face.shape[:2])
+            if CORRECT_COLOR:
+                warped_dst_img = warp_image_3d(dst_face, dst_points[:MAX_POINTS], 
+                                               src_points[:MAX_POINTS], src_face.shape[:2])
                 src_face = correct_colours(warped_dst_img, src_face, src_points)
             # Warp
             warped_src_face = warp_image_2d(src_face, transformation_from_points(dst_points, src_points), (w, h, 3))
@@ -195,7 +200,7 @@ def insert_face():
         mask = np.asarray(mask*mask_src, dtype=np.uint8)
 
         ## Correct color
-        if not warp_2d and correct_color:
+        if not WARP_2D and CORRECT_COLOR:
             warped_src_face = apply_mask(warped_src_face, mask)
             dst_face_masked = apply_mask(dst_face, mask)
             warped_src_face = correct_colours(dst_face_masked, warped_src_face, dst_points)
@@ -207,10 +212,7 @@ def insert_face():
 
         x, y, w, h = dst_shape
         result_bboxs.append(dst_shape)
-        
-        #dst_img_cp = CROWD.copy()
         CROWD[y:y+h, x:x+w] = output
-        #output = dst_img_cp
         
 
     output_labeled = CROWD.copy()
@@ -271,6 +273,12 @@ PREDICTOR = dlib.shape_predictor('./models/shape_predictor_68_face_landmarks.dat
 SP = dlib.shape_predictor("./models/shape_predictor_5_face_landmarks.dat")
 FACEREC = dlib.face_recognition_model_v1("./models/dlib_face_recognition_resnet_model_v1.dat")
 
+MAX_SIZE_SELFIE = 400 
+MAX_SIZE_CROWD = 1000
+WARP_2D = False
+CORRECT_COLOR = True
+MAX_POINTS = 58
+
 app = Flask(__name__)
 app.debug=True
 app.config['UPLOAD_FOLDER'] = os.path.basename('static')
@@ -283,7 +291,6 @@ def hello_world():
 
 def read_image_exif(stream):
     image=Image.open(stream)
-
     try: 
         for orientation in ExifTags.TAGS.keys():
             if ExifTags.TAGS[orientation]=='Orientation':
@@ -297,36 +304,37 @@ def read_image_exif(stream):
         elif exif[orientation] == 8:
             image=image.rotate(90, expand=True)
 
-        # convert to opencv format
-        cv_image = np.array(image.convert('RGB'))
-        image.close()
-        cv_image = cv_image[:, :, ::-1].copy()
-        return cv_image
-
     except (AttributeError, KeyError, IndexError):
         # cases: image don't have getexif
-        image.close()
-        return image
+        pass
+
+    # convert to opencv format
+    cv_image = np.array(image.convert('RGB'))
+    image.close()
+    cv_image = cv_image[:, :, ::-1].copy()
+    return cv_image
 
 
 @app.route('/create_mix',  methods=['GET', 'POST'])
 def upload_create_mix():
 
-    file = request.files['image_crowd']
-    global CROWD
-    CROWD = read_file_buffer(file)
-    global ME
+    CROWD = read_image_exif(request.files['image_crowd'].stream)
     ME = read_image_exif(request.files['image_selfie'].stream)
 
     start = time.time() 
-    ME = open_img(ME, biggest=400)
+    ME = open_img(ME, biggest=MAX_SIZE_SELFIE)
+    print(f" [INFO] Selfie shape: {ME.shape}")
 
     old_shape = CROWD.shape[:-1][::-1]
-    CROWD = open_img(CROWD, biggest=1000) 
+    CROWD = open_img(CROWD, biggest=MAX_SIZE_CROWD) 
+    print(f" [INFO] Crowd shape: {CROWD.shape}")
 
-    #mix 
-    output_labeled = insert_face()
-    if output_labeled is None: 
+    #MIX
+    #preprocess_img gets one photo of the crowd and a list of (selfies, points) 
+    result = preprocess_img.run(CROWD, [(ME, None)])
+    output_labeled = insert_face(result, CROWD)
+    if output_labeled is None:
+        print(" [INFO] Something goes wrong :( ")
         return render_template('index.html', created_success=False, init=True)
 
     CROWD = cv2.resize(CROWD, old_shape, Image.LANCZOS)
@@ -342,16 +350,7 @@ def upload_create_mix():
     # save labeled answer
     cv2.imwrite(file + "/answer.jpeg", cv2.cvtColor(output_labeled, cv2.COLOR_RGB2BGR))
 
-    # update globals
-    ME = None; CROWD = None
-
     result_filename = url_for('static', filename='result.jpeg') + '?rnd=' + str(random.randint(0, 10e9))
     answer_filename = url_for('static', filename='answer.jpeg') + '?rnd=' + str(random.randint(0, 10e9))
     return render_template('index.html', created_success=True, init=True,
                            result_filename=result_filename, answer_filename=answer_filename)
-
-
-@app.route('/', methods=['GET'])
-@crossdomain(origin='*')
-def healthcheck():
-    return "OK"
